dc779733e08e5adc8c7e12628baa53a7d757b524
Raphael Barbosa <raphaelcorreabarbosa@hotmail.com>
2026-03-24 16:55:41 -0300

    v1.1.3: retry keybind, port guard, active agents fix, ghost text update

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
